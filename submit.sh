e085a6edae5ec24887d02c9ab29167c987fd6bdb
Robert Gower <gowerrobert@gmail.com>
2025-03-20 11:19:00 -0400

    update shake config and results

---
+++ submit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CONFIG_NAME=$(basename "$1" .yaml)
+
+sbatch <<EOF
+#!/bin/bash
+#SBATCH -J ${CONFIG_NAME}
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --time=150:00:00
+#SBATCH --partition=gpu
+#SBATCH --constraint=a100
+#SBATCH -o output/slurm_logs/${CONFIG_NAME}.log
+
+module load python
+
+# Activate environment
+source gptopt/bin/activate
+
+# Install the necessary packages
+python3.9 -m pip install -e .
+
+# Run the Python script with the config file
+python3.9 run.py --config $1
+EOF
