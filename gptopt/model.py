625eb491535a8190adb13d91386c14d69149ab85
modichirag <cmodi@flatironinstitute.org>
2025-03-26 17:58:06 -0400

    clean(er) model loading

---
+++ gptopt/model.py
@@ -0,0 +1,29 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, AutoTokenizer, AutoModelForCausalLM
+
+def load_model_and_tokenizer(config, device):
+
+    if 'model_name' in config['gpt_model']:
+        print(f"Loading model {config['gpt_model']['model_name']} and ignoring other configurations if specified")
+        model = AutoModelForCausalLM.from_pretrained(config['gpt_model']['model_name'], device_map="auto").to(device)
+        tokenizer = AutoTokenizer.from_pretrained(config['gpt_model']['model_name'])
+        if not config['gpt_model']['pretrained']:
+            model_config = model.config
+            del model
+            model = GPT2LMHeadModel(model_config).to(device)
+        else:
+            print("Using pre-trained version")
+            
+    else:
+        gpt_config = config['gpt_model']
+        model_config = GPT2Config(
+            n_embd=gpt_config['n_embd'],   
+            n_layer=gpt_config['n_layer'],  
+            n_head=gpt_config['n_head'],    
+            vocab_size=gpt_config['vocab_size'], 
+        )
+        model = GPT2LMHeadModel(model_config).to(device)   # Initialize a new model with random weights using this configuration
+        print("Loading gpt2 tokenizer as default tokenizer\n")
+        tokenizer = AutoTokenizer.from_pretrained(config['gpt_model']['gpt2'])
+    tokenizer.pad_token = tokenizer.eos_token
+    return model, tokenizer
+
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import yaml
 import argparse
 import torch
 from torch.utils.data import DataLoader
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, AutoTokenizer, AutoModelForCausalLM
+# from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset, Dataset
 import matplotlib.pyplot as plt
 from gptopt.utils import compute_cross_entropy_loss, get_default_config, merge_configs, load_config, get_outputfile_from_configfile
@@ -10,46 +10,40 @@ from gptopt.data import load_data
 from gptopt.train import train
 from gptopt.optim.utils import get_scheduler, get_optimizer
 from gptopt.utils import set_seed
+from gptopt.model import load_model_and_tokenizer
 import copy 
 import json
 from gptopt.utils import hash_config
 import os
 
+
 def main(config_file=None):
+
     set_seed(42)
     default_config = get_default_config()      # Default parameters if no config file is provided
     if config_file:
         config = load_config(default_config, config_file)
     outputname = config_file.replace("configs/","").replace('.yaml','')
     output_dir = f"gptopt/outputs/{outputname}"
+    os.makedirs(output_dir, exist_ok=True)  
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
-    train_dataloader, test_dataloader = load_data(config['dataset']['name'], batch_size=config['training_params']['batch_size'])
-    # Load model using "model_name"
-    if 'model_name' in config['gpt_model']:
-        model = AutoModelForCausalLM.from_pretrained(config['gpt_model']['model_name'], device_map="auto").to(device)
-    else:
-        gpt_config = config['gpt_model']
-        model_config = GPT2Config(
-            n_embd=gpt_config['n_embd'],   # Hidden size used in distilgpt2
-            n_layer=gpt_config['n_layer'],    # Number of layers in distilgpt2
-            n_head=gpt_config['n_head'],    # Number of attention heads in distilgpt2
-            vocab_size=gpt_config['vocab_size'],  # Standard GPT-2 vocabulary size
-        )
-        model = GPT2LMHeadModel(model_config).to(device)   # Initialize a new model with random weights using this configuration
-    tokenizer = AutoTokenizer.from_pretrained(config['gpt_model']['tokenizer_name'])
-    tokenizer.pad_token = tokenizer.eos_token
 
+    # Load model
+    model, tokenizer = load_model_and_tokenizer(config, device)
+    
     # Set the training parameters
     training_params = config['training_params'] 
     print(f"Training on dataset {config['dataset']['name']}")
     # Access the optimizer parameters
     list_optimizer_params = config["optimizer_params"]
-    
-    os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+    # Load data
+    train_dataloader, test_dataloader = load_data(config['dataset']['name'], batch_size=config['training_params']['batch_size'])
 
     for optimizer_config in list_optimizer_params:
         for lr in optimizer_config['lr']:
+            print()
             print(f"Training with optimizer {optimizer_config['name']} and learning rate {lr}")
             model_copy = copy.deepcopy(model).to(device)  # The model remains the same
             total_iterations = training_params['num_epochs'] * len(train_dataloader)
