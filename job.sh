c465b8bb16fb2261e092c861c7b2943deabc3501
modichirag <cmodi@flatironinstitute.org>
2025-04-22 12:04:07 -0400

    job scripts

---
+++ job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --constraint=a100
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH -o logs/r%x.o%j
+
+export OMP_NUM_THREADS=1
+
+module load modules/2.3-20240529
+#module load cuda cudnn gcc
+source /mnt/home/cmodi/envs/torchlatest/bin/activate
+module list 
+
+#time torchrun --standalone --nproc_per_node=1 test_run.py --config configs/finewebmini.yaml
+time torchrun --standalone --nproc_per_node=1 run_distributed.py --config configs/shakespeare.yaml
