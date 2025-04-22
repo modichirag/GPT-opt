efe6e037363f5fbfa5ceffe1d67ec94bf5b22112
modichirag <cmodi@flatironinstitute.org>
2025-04-22 12:04:07 -0400

    job scripts

--- job4.sh
+++ job4.sh
@@ -5,7 +5,7 @@
 #SBATCH --constraint=h100
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH -o logs/r%x.o%j
 
 export OMP_NUM_THREADS=1
@@ -15,5 +15,7 @@ module load modules/2.3-20240529
 source /mnt/home/cmodi/envs/torchlatest/bin/activate
 module list 
 
-time torchrun --standalone --nproc_per_node=4 run_distributed.py --config configs/finewebmini.yaml
+#time torchrun --standalone --nproc_per_node=4 run_distributed.py --config configs/finewebmini.yaml 
+time torchrun --standalone --nproc_per_node=4 run_distributed.py --config configs/fineweb1B.yaml --suffix nognorm
+#time torchrun --standalone --nproc_per_node=4 run_distributed.py --config configs/fineweb10B.yaml
 #time torchrun --standalone --nproc_per_node=4 run_distributed.py --config configs/shakespeare-local.yaml
