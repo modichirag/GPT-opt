Fixed float16 bug in muon LR scaling, changed interface for muon scaling options.

gptopt/optim/muon.py
@@ -177,7 +177,12 @@ class Muon(torch.optim.Optimizer):
                 
                 # scale update
                 adjusted_lr = self.adjust_lr_for_muon(
-                    lr, group["rms_scaling"], group["nuclear_scaling"], p.shape, g, u
+                    lr,
+                    group["rms_scaling"],
+                    group["nuclear_scaling"],
+                    p.shape,
+                    g.bfloat16(), # convert to float16 to be compatible with u
+                    u
                 )
                 
                 # apply weight decay

gptopt/optim/utils.py
@@ -113,16 +113,18 @@ def get_optimizer(opt_config: dict, lr = 1e-3) -> Tuple[torch.optim.Optimizer, d
                   'use_fstar': True
                   }
 
-    elif name == 'muon':
+    elif 'muon' in name:
         opt_obj = Muon
+        rms_scaling = 'rms' in name
+        nuclear_scaling = 'nuclear' in name
         hyperp = {'lr': lr,
                   'wd': opt_config.get('weight_decay', 0),
                   'adamw_betas': opt_config.get('betas', (0.95, 0.95)),
                   'momentum': opt_config.get('momentum', 0.95),
                   'nesterov': True,
                   'ns_steps': opt_config.get('ns_steps', 5),
-                  'rms_scaling': opt_config.get('rms_scaling', True),
-                  'nuclear_scaling': opt_config.get('rms_scaling', False)
+                  'rms_scaling': rms_scaling,
+                  'nuclear_scaling': nuclear_scaling
                   }
 
     # elif name == 'iam':