7929791e7ffa3cf7493c9d1f7bdcba7b3d80298d
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2025-06-02 18:35:23 -0400

    Configs and runscript for fineweb1B experiments with Muon variations.

--- job4.sh
+++ job4.sh
@@ -10,12 +10,11 @@
 
 export OMP_NUM_THREADS=1
 
-module load modules/2.3-20240529
+#module load modules/2.3-20240529
 #module load cuda cudnn gcc
-source /mnt/home/cmodi/envs/torchlatest/bin/activate
-module list 
+#source /mnt/home/cmodi/envs/torchlatest/bin/activate
+#module list 
+module load python
+source gptopt/bin/activate
 
-#time torchrun --standalone --nproc_per_node=4 run.py --config configs/finewebmini.yaml 
-time torchrun --standalone --nproc_per_node=4 run.py --config configs/fineweb1B.yaml --suffix nognorm
-#time torchrun --standalone --nproc_per_node=4 run.py --config configs/fineweb10B.yaml
-#time torchrun --standalone --nproc_per_node=4 run.py --config configs/shakespeare-local.yaml
+time torchrun --standalone --nproc_per_node=4 run.py --config configs/fineweb1B_1.yaml
